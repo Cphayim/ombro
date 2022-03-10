ea9a3ecf776b5ebc8441bb2f2d51525c3ff80760
Cphayim <i@cphayim.me>
2022-03-11 00:12:21 +0800

    test: use @swc/jest replacement ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,22 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   projects: [
-    {
-      displayName: '@ombro/logger',
-      rootDir: 'packages/logger',
-      preset: 'ts-jest',
-      testMatch: ['<rootDir>/**/*.(test|spec).ts'],
-      resetMocks: true,
-    },
-    {
-      displayName: '@ombro/node',
-      rootDir: 'packages/node',
-      preset: 'ts-jest',
-      testMatch: ['<rootDir>/**/*.(test|spec).ts'],
-    },
+    tsProject({ displayName: '@ombro/logger', rootDir: 'packages/logger' }),
+    tsProject({ displayName: '@ombro/node', rootDir: 'packages/node' }),
   ],
+}
+
+function tsProject({ displayName, rootDir }, others = {}) {
+  return {
+    displayName,
+    rootDir,
+    transform: {
+      '^.+\\.tsx?$': ['@swc/jest'],
+    },
+    testMatch: ['<rootDir>/**/*.(test|spec).ts'],
+    ...others,
+  }
 }

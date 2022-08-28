d39dce5e31285e6c3080ca82f0dff1913881c255
Cphayim <i@cphayim.me>
2022-08-28 22:33:22 +0800

    chore(test): change the testing framework to vitest (#27)

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    root: __dirname,
+    environment: 'node',
+    include: [
+      'packages/**/__tests__/**/*.ts',
+      'packages/**/*.{spec,test}.ts',
+      '!**/*.d.ts', //
+    ],
+    coverage: {
+      include: ['packages/**/src/**/*.ts'],
+      reporter: ['text', 'lcov'],
+      statements: 90,
+    },
+  },
+})
